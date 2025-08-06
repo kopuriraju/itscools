c10cff4383a14df2fc556577be2bb362dbb3413d
kopuriraju <rajkopuri@gmail.com>
2025-08-06 10:17:09 +0530

    created release branch with required code for jenkins maven job

---
+++ src/main/java/myapp/App.java
@@ -0,0 +1,10 @@
+package myapp;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("good morning Devops engineers!");
+    }
+}
