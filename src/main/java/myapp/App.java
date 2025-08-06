e8fdcd7cc464cdef8081d9e195a8ce5798f69da6
kopuriraju <rajkopuri@gmail.com>
2025-08-06 11:00:47 +0530

    did the code changes

--- src/main/java/myapp/App.java
+++ src/main/java/myapp/App.java
@@ -5,6 +5,6 @@ package myapp;
  */
 public class App {
     public static void main(String[] args) {
-        System.out.println("good morning Devops engineers!");
+        System.out.println("good morning Cloudops/Devops engineers!");
     }
 }
